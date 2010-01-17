fea6d1e00a1108c4748db3a3b683ba4bd611141b
chdorner <chdorner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-17 14:34:27 +0000

    Example configuration data for tests
    git-svn-id: https://plugins.svn.wordpress.org/razuna-media-manager/trunk@194765 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ api/tests/Base.example.php
@@ -0,0 +1,11 @@
+<?php
+
+interface Base {
+	const CONFIG_HOST_NAME				= '';
+	const CONFIG_HOST_ID					= ;
+	const CONFIG_USERNAME					= '';
+	const CONFIG_PASSWORD_CLEAR		= '';
+	const CONFIG_PASSWORD_HASHED	= '';
+}
+
+?>
